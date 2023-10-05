f712cd435c7a8a39eae7d840c9aa33e94ffa14ec
boby <js903783@gmail.com>
2023-10-05 22:07:12 +0530

    removed auth

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -11,34 +11,35 @@ import Loader from 'components/Loader'
 export default function Routes() {
   const dispatch = useDispatch()
   const auth = useAppSelector((state) => state.auth)
-  React.useEffect(() => {
-    if (!auth.user) {
-      const unsubscribe = onAuthStateChanged(firebaseAuth, async (currentUser) => {
-        if (currentUser) {
-          dispatch(
-            setUser({
-              displayName: currentUser.displayName || '',
-              email: currentUser.email || '',
-              photoURL: currentUser.photoURL || '',
-              uid: currentUser.uid,
-              emailVerified: currentUser.emailVerified,
-              isAnonymous: currentUser.isAnonymous,
-              phoneNumber: currentUser.phoneNumber || '',
-              providerData: currentUser.providerData
-            })
-          )
-        }
-        dispatch(stopLoading())
-      })
-      return () => unsubscribe()
-    }
-  }, [auth])
+  // React.useEffect(() => {
+  //   if (!auth.user) {
+  //     const unsubscribe = onAuthStateChanged(firebaseAuth, async (currentUser) => {
+  //       if (currentUser) {
+  //         dispatch(
+  //           setUser({
+  //             displayName: currentUser.displayName || '',
+  //             email: currentUser.email || '',
+  //             photoURL: currentUser.photoURL || '',
+  //             uid: currentUser.uid,
+  //             emailVerified: currentUser.emailVerified,
+  //             isAnonymous: currentUser.isAnonymous,
+  //             phoneNumber: currentUser.phoneNumber || '',
+  //             providerData: currentUser.providerData
+  //           })
+  //         )
+  //       }
+  //       dispatch(stopLoading())
+  //     })
+  //     return () => unsubscribe()
+  //   }
+  // }, [auth])
 
   return (
     <div>
-      {auth.loading && <Loader loading />}
+      <PrivateRouter />
+      {/* {auth.loading && <Loader loading />}
       {auth.user && !auth.loading && <PrivateRouter />}
-      {!auth.user && !auth.loading && <PublicRouter />}
+      {!auth.user && !auth.loading && <PublicRouter />} */}
     </div>
   )
 }
